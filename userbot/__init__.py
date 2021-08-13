4f9095d6f660ad6fb56ed96ca61c70f46b78d46c
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-08-13 04:23:04 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -22,6 +22,8 @@ catversion = "3.0.3"
 
 if Config.UPSTREAM_REPO == "ODY":
     UPSTREAM_REPO_URL = "https://github.com/Odysho/Novuserbot"
+elif Config.UPSTREAM_REPO == "goodcat":
+    UPSTREAM_REPO_URL = "https://github.com/sandy1709/catuserbot"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 

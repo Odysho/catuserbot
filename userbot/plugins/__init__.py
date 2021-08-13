77114f8713ef56a32fc4b0634db6c9098daff347
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-08-13 03:57:50 +0300

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -18,7 +18,7 @@ from ..helpers import *
 from ..helpers.utils import _cattools, _catutils, _format, install_pip, reply_id
 
 # =================== CONSTANT ===================
-bot = catub
+bot = november
 LOGS = logging.getLogger(__name__)
 USERID = catub.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
 ALIVE_NAME = Config.ALIVE_NAME
@@ -33,7 +33,7 @@ HEROKU_API_KEY = Config.HEROKU_API_KEY
 
 thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
 
-USERID = catub.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
+USERID = november.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
 
 # mention user
 mention = f"[{Config.ALIVE_NAME}](tg://user?id={USERID})"
